31ab8df672a1d249ae9b0450b10a7703b94d385c
Hal-ang <jpjgv05@gmail.com>
2020-12-01 14:14:07 +0900

    disigned Contact page

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-    return <div>Contact 됐냐?</div>;
+    return (
+      <div id="contact-area">
+        <div id="contact-title">CONTACT</div>
+        <div id="writing-area">
+          <input
+            id="writing-email"
+            type="email"
+            placeholder="이메일을 입력하세요"
+          ></input>
+          <button id="submit-btn">요청하기</button>
+        </div>
+        <div id="detail-contact">
+          요청하기를 누르면 이메일로 이력서가 전송되어 확인 가능합니다!
+        </div>
+      </div>
+    );
   }
 }
 
